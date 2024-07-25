4c023a39b7383516bdecc6082d131f95109a6efc
vitaliy_kozhushko <itvkip@yandex.ru>
2024-07-25 11:32:06 +0300

    Сайт запускается через Docker

--- initial_project_django/settings.py
+++ initial_project_django/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'NAME': env('DB_NAME'),
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PASSWD'),
-        'HOST': 'localhost',
+        'HOST': env('DB_HOST'),
         'PORT': env('DB_PORT'),
     }
 }
